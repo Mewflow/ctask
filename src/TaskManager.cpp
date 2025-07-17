7d4343767c17550a25e7ea17dc2e8ab7b70cf031
Mewflow <beko365x@gmail.com>
2025-07-17 13:13:59 +0200

    add implementation of the DeleteTask function

--- src/TaskManager.cpp
+++ src/TaskManager.cpp
@@ -29,3 +29,29 @@ void TaskManager::CreateNewTask()
     taskList.push_back(newTask);
     std::cout << "\nTask: " << newTask.GetTaskTitle() << " was created successfully!\n";
 }
+
+/*
+    Gets the title of the Task from the user and iterates through the list to delete it.
+*/
+void TaskManager::DeleteTask()
+{
+    std::cout << "Enter the title of the Task you want to delete: ";
+    std::string taskToDelete{""};
+    std::cin >> taskToDelete;
+
+    // Search the TaskList for this entry and remove it
+    for(auto it = taskList.begin(); it < taskList.end(); it++ )
+    {    
+        if(it->GetTaskTitle() == taskToDelete)
+        {
+            taskList.erase(it);
+            std::cout << "Deleted task: " << taskToDelete << " successfully." << std::endl;
+            return;
+        }
+
+        else 
+        {
+            std::cout << "Couldn't find task: " << taskToDelete << ". Returning to main menu.\n" << std::endl;
+        }
+    }
+}
